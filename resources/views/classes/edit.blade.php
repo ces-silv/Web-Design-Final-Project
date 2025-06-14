8cfa47a088ade394246a17e7a49c8aa4b318b66d
nestars16 <nesmb16@gmail.com>
2025-06-14 15:47:29 -0600

    Adding class views and editing navigation

---
+++ resources/views/classes/edit.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex items-center gap-2">
+            <i class="fa-solid-book text-2xl text-[#31c0d3] dark:text-[#31c0d3] mr-2"></i>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-100">
+                {{ __('Editar Clase') }}
+            </h2>
+        </div>
+    </x-slot>
+    <div class="min-h-screen py-12 bg-white/10 dark:bg-gray-900 transition-colors">
+        <div class="max-w-lg mx-auto bg-white/80 dark:bg-gray-800/80 border border-[#0b545b]/20 dark:border-gray-700 shadow-lg p-8 rounded-2xl">
+            @include('classes._form', ['class' => $class])
+        </div>
+    </div>
+</x-app-layout>
