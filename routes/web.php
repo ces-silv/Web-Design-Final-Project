fcd21b8ad7ae04768b899e15b120af405bcc05ac
Jareth <jlopezv@uamv.edu.ni>
2025-06-17 17:34:50 -0600

    Revert "Merge pull request #10 from ces-silv/feat/Crud_Facultad"
    This reverts commit 6a58d529301a9afe94c0ca9e3c58da3042b48e8a, reversing
    changes made to 1238e450f1acea3e23a9352fa1427bc1d00489f8.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProfessorController;
 use App\Http\Controllers\ClassController;
-use App\Http\Controllers\FacultyController;
 use App\Http\Controllers\JustificationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JustificationController;
@@ -33,7 +32,6 @@ Route::middleware(['auth', 'role:user,admin'])->group(function () {
     Route::resource('justifications', JustificationController::class);
     Route::view('/about', 'pages.about')
     ->name('about');
-    Route::resource('faculties', FacultyController::class);
     Route::post('/available-classes', [\App\Http\Controllers\JustificationController::class, 'getAvailableClasses']);
 });
 
